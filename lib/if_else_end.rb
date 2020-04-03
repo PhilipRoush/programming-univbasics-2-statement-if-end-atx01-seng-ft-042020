c4341367f07d93a4e79725ba9af2ad30a604d863
Philip Roush <roush.philip@gmail.com>
2020-04-03 21:33:26 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,4 +1,9 @@
 # Write your solution
-run_code_inside = true
-puts "Code before if...end"
+current_time = Time.now
+current_time = current_time.to_i
 
+if current_time % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
